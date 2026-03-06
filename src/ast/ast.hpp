0ba349c4c740e45d8256db0bb9c0e75454835b67
Uttkarsh Pawan <utkbest@gmail.com>
2026-03-06 16:50:50 +0530

    feat: declare ASTs

---
+++ src/ast/ast.hpp
@@ -0,0 +1,61 @@
+#include<string>
+#include<vector>
+
+// ExprAST -  base class for all expression nodes.
+class ExprAST {
+public:
+    virtual ~ExprAST() = default;
+};
+
+// NumberExprAST - Expression class for numeric literals
+class NumberExprAST : public ExprAST {
+    double Val;
+
+public: 
+    NumberExprAST(double Val) : Val(Val) {}
+};
+
+// VariableExprAST - Expression class for referencing a variable
+class VariableExprAST : public ExprAST {
+    std::string Name;
+
+public:
+    VariableExprAST(const std::string &Name) : Name(Name) {}
+};
+
+// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public ExprAST {
+    char Op;
+    std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+    BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS, std::unique_ptr<ExprAST> RHS) : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+// CallExprAST - Expression class for function calls
+class CallExprAST : public ExprAST {
+    std::string Callee;
+    std::vector<std::unique_ptr<ExprAST>> Args;
+
+public:
+    CallExprAST(const std::string &Callee, std::vector<std::unique_ptr<ExprAST>> Args) : Callee(Callee), Args(std::move(Args)) {}
+};
+
+// prototype for the function
+class ProtoypeAST {
+    std::string Name;
+    std::vector<std::string> Args;
+
+public:
+    ProtoypeAST(std::string &Name, std::vector<std::string> Args): Name(Name), Args(std::move(Args)) {}
+    const std::string getName() const { return Name; }
+};
+
+// function definition
+class FunctionAST {
+    std::unique_ptr<ProtoypeAST> Proto;
+    std::unique_ptr<ExprAST> Body;
+
+public:
+    FunctionAST(std::unique_ptr<ProtoypeAST> Proto, std::unique_ptr<ExprAST> Body) : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
